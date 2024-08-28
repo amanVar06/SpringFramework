18d7f2332bd5a04ddd3ebe46417734086c1c48ba
Aman Varshney <amanvarshney212@gmail.com>
2024-08-28 17:02:43 +0530

    Refactoring changes.

--- src/main/java/com/amanv/Dev.java
+++ src/main/java/com/amanv/Dev.java
@@ -2,47 +2,47 @@ package com.amanv;
 
 public class Dev {
 
-    private Laptop laptop;
+//    private Laptop laptop;
     private Computer computer;
-    private int age;
+//    private int age;
 
     public Dev() {
         System.out.println("Dev constructor..");
     }
 
-    public Dev(int age) {
-        this.age = age;
-        System.out.println("Dev parameterized constructor..");
-    }
+//    public Dev(int age) {
+//        this.age = age;
+//        System.out.println("Dev parameterized constructor..");
+//    }
 
-    public Dev(Laptop laptop) {
-        this.laptop = laptop;
-        System.out.println("Dev parameterized laptop constructor..");
-    }
+//    public Dev(Laptop laptop) {
+//        this.laptop = laptop;
+//        System.out.println("Dev parameterized laptop constructor..");
+//    }
 
-    public Computer getComputer() {
-        return computer;
-    }
+//    public Computer getComputer() {
+//        return computer;
+//    }
 
     public void setComputer(Computer computer) {
         this.computer = computer;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Laptop getLaptop() {
-        return laptop;
-    }
-
-    public void setLaptop(Laptop laptop) {
-        this.laptop = laptop;
-    }
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public Laptop getLaptop() {
+//        return laptop;
+//    }
+//
+//    public void setLaptop(Laptop laptop) {
+//        this.laptop = laptop;
+//    }
 
     public void build() {
 //        laptop.compile();
